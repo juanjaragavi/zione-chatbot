cb9c07503c1ca325b3f85c6b614f7b28f45bca0b
JuanJaramillo <juan.miguel.jaramillo1984@gmail.com>
2023-10-12 07:00:33 -0500

    Chat button translated

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,7 +51,7 @@ for message in st.session_state.messages:
 
 def clear_chat_history():
     st.session_state.messages = [{"role": "assistant", "content": "Ok, empecemos de nuevo. 😄"}]
-st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
+st.sidebar.button('Borra el historial del Chat', on_click=clear_chat_history)
 
 # Function for generating LLaMA2 response. Refactored from https://github.com/a16z-infra/llama2-chatbot
 def generate_ziomara_response(prompt_input):
